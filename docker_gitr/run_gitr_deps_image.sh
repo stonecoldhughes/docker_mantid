d1e2290ad8b0bd58e35c8a8ad1414bd87707cf65
null <null>
2021-01-26 18:35:54 -0500

    cloneless

--- docker_gitr/run_gitr_deps_image.sh
+++ docker_gitr/run_gitr_deps_image.sh
@@ -1,1 +1,6 @@
-docker run -i -t --rm --network host --name gitr_deps_container gitr_deps_image 
+docker run -i -t --rm \
+--network host \
+--name gitr_deps_container \
+-v "$(pwd)"/GITR:/GITR \
+-v "$(pwd)"/output:/output \
+gitr_deps_image 

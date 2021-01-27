ce3e16a5465babc2beb97175a1d356539f60526b
null <null>
2021-01-27 15:05:38 -0500

    making this workflow more flexible

--- build_gitr_chain.sh
+++ build_gitr_chain.sh
@@ -1,3 +1,4 @@
 bash build_dev_image.sh
-bash docker_gitr/build_gitr_deps_image.sh
-bash docker_gitr/build_gitr_src_image.sh
+cd docker_gitr
+bash build_gitr_deps_image.sh
+bash build_gitr_src_image.sh "$1"
--- docker_gitr/build_gitr_src_image.sh
+++ docker_gitr/build_gitr_src_image.sh
@@ -1,1 +1,2 @@
-DOCKER_BUILDKIT=1 docker build --file gitr_src.df --network host --tag gitr_src_image --ssh default=/home/5n4/.ssh/key.priv .
+DOCKER_BUILDKIT=1 docker build --file gitr_src.df --network host --tag gitr_src_image --ssh default="$1" .
+# positional argument is full path to ssh private key file

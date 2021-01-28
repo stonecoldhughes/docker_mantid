b3097b4a6b26e73fc1e9176bc916fff0bb9f92dd
null <null>
2021-01-28 16:05:57 -0500

    Using secrets to inject the repo access token into build. Using env to pass in repo access token for pushing and pulling once inside the container.

--- docker_gitr/build_gitr_src_image.sh
+++ docker_gitr/build_gitr_src_image.sh
@@ -2,6 +2,10 @@ DOCKER_BUILDKIT=1 \
 docker build \
 --file gitr_src.df \
 --network host \
---build-arg REPO_ACCESS_TOKEN=$REPO_ACCESS_TOKEN
---tag gitr_src_image
-# positional argument is full path to ssh private key file
+--secret id=repo_access_token,src=$1 \
+--build-arg REPO_ACCESS_TOKEN=$REPO_ACCESS_TOKEN \
+--tag gitr_src_image .
+# You should check to make sure the repo access token actually exists... set it from the
+# file and add it before DOCKER_BUILDKIT
+# The secret is passed in as a file, the first argument should be repo_access_token.txt
+
--- docker_gitr/run_gitr_src_image.sh
+++ docker_gitr/run_gitr_src_image.sh
@@ -1,6 +1,6 @@
 docker run -i -t --rm \
 --network host \
 --name gitr_src_container \
--v /home/5n4/docker_gitr/output:/output \
---env-file ~/repo_access_token.txt \
+-v $(pwd)/output:/output \
+--env-file /home/5n4/repo_access_token.txt \
 gitr_src_image

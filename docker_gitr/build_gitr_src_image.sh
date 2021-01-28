4428bb717d53237090fa4c32c3824ea69518a47e
null <null>
2021-01-28 13:25:40 -0500

    changing to https

--- docker_gitr/build_gitr_src_image.sh
+++ docker_gitr/build_gitr_src_image.sh
@@ -1,2 +1,7 @@
-DOCKER_BUILDKIT=1 docker build --file gitr_src.df --network host --tag gitr_src_image --ssh default="$1" .
+DOCKER_BUILDKIT=1 \
+docker build \
+--file gitr_src.df \
+--network host \
+--build-arg REPO_ACCESS_TOKEN=$REPO_ACCESS_TOKEN
+--tag gitr_src_image
 # positional argument is full path to ssh private key file
--- docker_gitr/run_gitr_src_image.sh
+++ docker_gitr/run_gitr_src_image.sh
@@ -1,1 +1,6 @@
-docker run -i -t --rm --network host --name gitr_src_container -v /home/5n4/docker_gitr/output:/output gitr_src_image
+docker run -i -t --rm \
+--network host \
+--name gitr_src_container \
+-v /home/5n4/docker_gitr/output:/output \
+--env-file ~/repo_access_token.txt \
+gitr_src_image

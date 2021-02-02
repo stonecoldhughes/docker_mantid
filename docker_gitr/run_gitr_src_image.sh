d43bed3655a2d000d085ffbbcfb44119253191d7
null <null>
2021-02-02 18:56:49 -0500

    Fixed development docker image for GITR. Next, you'll need to see if you can use Ninja instead of Make. Take out mantid stuff... Looks like that can always be run on the analysis cluster.

--- docker_gitr/run_gitr_src_image.sh
+++ docker_gitr/run_gitr_src_image.sh
@@ -2,8 +2,8 @@ docker run -i -t --rm \
 --network host \
 --name gitr_src_container \
 -v $(pwd)/output:/output \
+--env-file $1 \
 gitr_src_image
 
 
 # This is probably no longer needed
-# --env-file /home/5n4/repo_access_token.txt \
